ffd8deb61b4efbcb82ef059d48d70b7f0c2c55cc
anjaniganapathy <anjani.ganapathy@gmail.com>
2025-09-17 16:58:28 -0400

    Un-commented all experiments; added line to create data directory when running main.py

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ from utils import read_conllu_file, item_indexer
 from experiment import experiment
 
 # download Con-LLU files containing data
-# data_dir = "hindi-pos-tagging-vqvae/data"
-os.mkdir('data')
+if not os.path.isdir("data"):
+    os.mkdir('data')
+
 train_hi_url = 'https://raw.githubusercontent.com/UniversalDependencies/UD_Hindi-HDTB/refs/heads/master/hi_hdtb-ud-train.conllu'
 train_filename = "data/hi_hdtb-ud-train.conllu"
 dev_hi_url = 'https://raw.githubusercontent.com/UniversalDependencies/UD_Hindi-HDTB/refs/heads/master/hi_hdtb-ud-dev.conllu'
@@ -27,7 +28,7 @@ config = Config()
 
 # instantiate device
 curr_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(curr_device)
+# print(curr_device)
 
 train_sents, train_labels = read_conllu_file("data/hi_hdtb-ud-train.conllu")
 dev_sents, _ = read_conllu_file("data/hi_hdtb-ud-dev.conllu")
@@ -47,44 +48,44 @@ train_dataset = POSDataset(train_sents, tokenizer, config)
 dev_dataset = POSDataset(dev_sents, tokenizer, config)
 test_dataset = POSDataset(test_sents, tokenizer, config)
 
-# baseline_decoder = DecoderBiLSTM(
-#     tag_dim=config.tag_dim,
-#     dec_hidden=config.dec_hidden,
-#     vocab_size=len(v2i),
-#     num_layers=config.dec_layers
-# )
-#
-# ffn_decoder = DecoderFFN(
-#     tag_dim=config.tag_dim,
-#     vocab_size=len(v2i)
-# )
-#
-# char_decoder = CharDecoder(
-#     tag_dim=config.tag_dim,
-#     hidden_size=config.dec_hidden,
-#     char_vocab_size=len(c2i),
-#     max_char_len=config.max_word_len,
-#     device=curr_device
-# )
-#
+baseline_decoder = DecoderBiLSTM(
+    tag_dim=config.tag_dim,
+    dec_hidden=config.dec_hidden,
+    vocab_size=len(v2i),
+    num_layers=config.dec_layers
+)
+
+ffn_decoder = DecoderFFN(
+    tag_dim=config.tag_dim,
+    vocab_size=len(v2i)
+)
+
+char_decoder = CharDecoder(
+    tag_dim=config.tag_dim,
+    hidden_size=config.dec_hidden,
+    char_vocab_size=len(c2i),
+    max_char_len=config.max_word_len,
+    device=curr_device
+)
+
 # # Each of the experiments below runs a specific experiment for the thesis. Run each one as desired.
-#
+
 # Baseline model
-# config.use_char_architecture = False
-# decoder = (baseline_decoder, None)
-# bl_model, bl_loss_values, bl_dev_values, bl_div_loss_values, bl_vocab_reconstr_values, _ = experiment(config, "Baseline", decoder, v2i, i2v, l2i, i2l, c2i, i2c, train_dataset, dev_dataset, test_dataset, curr_device)
-#
+config.use_char_architecture = False
+decoder = (baseline_decoder, None)
+bl_model, bl_loss_values, bl_dev_values, bl_div_loss_values, bl_vocab_reconstr_values, _ = experiment(config, "Baseline", decoder, v2i, i2v, l2i, i2l, c2i, i2c, train_dataset, dev_dataset, test_dataset, curr_device)
+
 # FFN Decoder model
-# config.use_char_architecture = False
-# decoder = (ffn_decoder, None)
-# ffn_model, ffn_loss_values, ffn_dev_values, ffn_div_loss_values, ffn_vocab_reconstr_values, _ = experiment(config, "FFN Decoder", decoder, v2i, i2v, l2i, i2l, c2i, i2c, train_dataset, dev_dataset, test_dataset, curr_device)
-#
+config.use_char_architecture = False
+decoder = (ffn_decoder, None)
+ffn_model, ffn_loss_values, ffn_dev_values, ffn_div_loss_values, ffn_vocab_reconstr_values, _ = experiment(config, "FFN Decoder", decoder, v2i, i2v, l2i, i2l, c2i, i2c, train_dataset, dev_dataset, test_dataset, curr_device)
+
 # + Char Embeddings model
-# config.use_char_architecture = True
-# cemb_model, cemb_loss_values, cemb_dev_values, cemb_div_loss_values, cemb_vocab_reconstr_values, _ = experiment(config, "Char Embeddings", decoder, v2i, i2v, l2i, i2l, c2i, i2c, train_dataset, dev_dataset, test_dataset, curr_device)
-#
+config.use_char_architecture = True
+cemb_model, cemb_loss_values, cemb_dev_values, cemb_div_loss_values, cemb_vocab_reconstr_values, _ = experiment(config, "Char Embeddings", decoder, v2i, i2v, l2i, i2l, c2i, i2c, train_dataset, dev_dataset, test_dataset, curr_device)
+
 # + Char Decoder model
-# config.use_char_architecture = True
-# config.vocab_loss_weight = 0.5
-# decoder = (ffn_decoder, char_decoder)
-# cdec_model, cdec_loss_values, cdec_dev_values, cdec_div_loss_values, cdec_vocab_reconstr_values, cdec_char_reconstr_values = experiment(config, "Char Decoder", decoder, v2i, i2v, l2i, i2l, c2i, i2c, train_dataset, dev_dataset, test_dataset, curr_device)
+config.use_char_architecture = True
+config.vocab_loss_weight = 0.5
+decoder = (ffn_decoder, char_decoder)
+cdec_model, cdec_loss_values, cdec_dev_values, cdec_div_loss_values, cdec_vocab_reconstr_values, cdec_char_reconstr_values = experiment(config, "Char Decoder", decoder, v2i, i2v, l2i, i2l, c2i, i2c, train_dataset, dev_dataset, test_dataset, curr_device)
